152fa5b1e0e519a1cab38627be5260c541aafb2e
Kirnobi <ethan.shi@gmail.com>
2023-11-08 00:01:04 -0500

    Scene Transitions / Walk Buttons (URP Installed)

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -16,23 +16,67 @@ public class GameManager : MonoBehaviour
     public GameObject MapPanel;
     public GameObject SallyPanel;
 
+    public GameObject currentRoom;
+
 
     //Initialize variables
     bool LoadPanel = false;
+    bool walkMode = true;
+    public List<GameObject> walkButtons = new List<GameObject>(); //List of game objects to be iterated through when walkmode is turned on
 
+    //deactivate all walk buttons on game start.
+    private void Start()
+    {
+        walkModeToggle();
+    }
 
     //Main Method
     private void Update()
     {
+        transform.position = currentRoom.transform.position;
         //Loading Panels
         if (LoadPanel)
         {
-            Panel.transform.position = Vector3.Lerp(Panel.transform.position, new Vector3(0, 0, 0), Time.deltaTime * 5);
+            Panel.transform.position = Vector3.Lerp(Panel.transform.position, transform.position, Time.deltaTime * 5);
         }
 
         else
         {
-            Panel.transform.position = Vector3.Lerp(Panel.transform.position, new Vector3(-18, 0, 0), Time.deltaTime * 5);
+            Panel.transform.position = Vector3.Lerp(Panel.transform.position, transform.position + new Vector3(-18, 0, 0), Time.deltaTime * 5);
+        }
+    }
+
+    //Coroutine for screen transition
+    //Loops for 2 seconds, lowering opacity, swaps currentRoom object, and makes the new object visible.
+    IEnumerator roomTransition(GameObject room)
+    {
+        Time.timeScale = 0;
+        for (float i = 1; i >= 0; i -= Time.unscaledDeltaTime/3*2)
+        {
+            currentRoom.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, i);
+            yield return null;
+        }
+        currentRoom = room;
+        for (float i = 0; i < 1; i += Time.unscaledDeltaTime/3*2)
+        {
+            currentRoom.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, i);
+            yield return null;
+        }
+        Time.timeScale = 1;
+    }
+
+    public void moveToRoom(GameObject room)
+    {
+        StartCoroutine(roomTransition(room));
+    }
+    //Method to toggle walking mode. Runs through for loop to set buttons to active, and deactivates them when walk mode is toggled again.
+
+    public void walkModeToggle()
+    {
+        walkMode = !walkMode;
+        foreach (GameObject button in walkButtons)
+        {
+            button.SetActive(walkMode);
         }
     }
 
