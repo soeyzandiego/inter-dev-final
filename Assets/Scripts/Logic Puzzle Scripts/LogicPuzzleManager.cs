46812b63af02030ed90e14227a0813a7d5787c5c
Kirnobi <ethan.shi@gmail.com>
2023-12-12 18:32:48 -0500

    LogicPuzzleManager now has an OnDestroy() that destroys all associated objects when the object is destroyed.

--- Assets/Scripts/Logic Puzzle Scripts/LogicPuzzleManager.cs
+++ Assets/Scripts/Logic Puzzle Scripts/LogicPuzzleManager.cs
@@ -25,20 +25,23 @@ public class LogicPuzzleManager : MonoBehaviour
     [SerializeField] GameObject[] logicObjects = new GameObject[8]; // Reference to logicObjects (possibly not necessary?)
     [SerializeField] Vector2 hLobjectOffset = new Vector3(3, 0, 0); // horizontal spacing for instantiating logic objects.
     [SerializeField] Vector2 vLobjectOffset = new Vector3(0, 1.75f, 0);  // vertical spacing for instantiating logic objects.
+    [SerializeField] GameObject confirmButton;
+    [SerializeField] GameObject question;
+    [SerializeField] GameObject background;
     [SerializeField] GameObject canvas; // associated canvas object's game object.
 
     void Start()
     {
-        GameObject confirmButton = Instantiate(confirmButtonPrefab, canvas.transform);
-        confirmButton.GetComponent<Button>().onClick.AddListener(() => { CheckCorrect(); }) ;
-        confirmButton.transform.position = confirmButton.transform.position - (Vector3) vLobjectOffset;
-        Instantiate(logicPuzzleBGPrefab, canvas.transform);
-        GameObject question = Instantiate(textPrefab, canvas.transform);
+        confirmButton = Instantiate(confirmButtonPrefab, canvas.transform);
+        confirmButton.GetComponent<Button>().onClick.AddListener(() => { CheckCorrect(); });
+        confirmButton.transform.position = confirmButton.transform.position - (Vector3)vLobjectOffset;
+        background = Instantiate(logicPuzzleBGPrefab, canvas.transform);
+        question = Instantiate(textPrefab, canvas.transform);
         question.transform.position = new Vector3(0, 4, 0);
         question.GetComponent<TMP_Text>().text = logicQuestion;
         int count = 0;
         // Instantiating logic object prefabs (the draggable evidence pieces)
-        for(int i = 0; i < 2; i++)
+        for (int i = 0; i < 2; i++)
         {
             for (int j = 0; j < 2; j++)
             {
@@ -47,7 +50,7 @@ public class LogicPuzzleManager : MonoBehaviour
                 temp.transform.position = hLobjectOffset * (j - 1.5f) - Vector2.right + vLobjectOffset * (i + 0.25f);
                 //temp.GetComponent<SpriteRenderer>().sprite = logicObjectSprites[count]; Commented out for testing. PLEASE UNCOMMENT THIS WHEN U NEED TO PUT IN UR DESCRIPTIONS N WHATNOT.
                 temp.GetComponentInChildren<TMP_Text>().text = logicObjectDesc[count];
-                count++; 
+                count++;
                 temp.evidenceNum = count; //assigns evidence number identifier, to be used when checking the logic puzzle for correctness.
             }
 
@@ -64,13 +67,13 @@ public class LogicPuzzleManager : MonoBehaviour
         }
 
         count = 0;
-        for(int i = 0; i < 2; i++)
+        for (int i = 0; i < 2; i++)
         {
             logicSlot[i] = Instantiate(logicSlotPrefab, canvas.transform);
             logicSlot[i].transform.position = new Vector3((i - .5f) * 6 - 2f, -3.5f, 1);
             logicSlot[i].GetComponent<LogicSlot>().rightEvidence = correctEvidence[i * 2];
-            
-            
+
+
             logicWheel[i] = Instantiate(logicWheelPrefab, canvas.transform);
             logicWheel[i].transform.position = new Vector3((i - .5f) * 6 + 1f, -3.5f, 1);
             logicWheel[i].GetComponent<LogicWheel>().rightEvidence = correctEvidence[i * 2 + 1];
@@ -82,7 +85,7 @@ public class LogicPuzzleManager : MonoBehaviour
         }
 
     }
-    
+
     private void Update()
     {
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
@@ -110,7 +113,7 @@ public class LogicPuzzleManager : MonoBehaviour
 
     public bool CheckCorrect()
     {
-        for (int i = 0; i < 2; i++) 
+        for (int i = 0; i < 2; i++)
         {
             LogicSlot tempSlot = logicSlot[i].GetComponent<LogicSlot>();
             LogicWheel tempWheel = logicWheel[i].GetComponent<LogicWheel>();
@@ -121,5 +124,24 @@ public class LogicPuzzleManager : MonoBehaviour
         }
         Debug.Log("Works");
         return true;
+    }
+
+    private void OnDestroy()
+    {
+        foreach (GameObject o in logicSlot)
+        {
+            Destroy(o);
+        }
+        foreach (GameObject o in logicWheel)
+        {
+            Destroy(o);
+        }
+        foreach (GameObject o in logicObjects)
+        {
+            Destroy(o);
+        }
+        Destroy(question);
+        Destroy(confirmButton);
+        Destroy(background);
     }
 }
